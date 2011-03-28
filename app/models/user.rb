277517c2522f33910862a50545fb2995975502a8
Brian Zollinhofer <brian.zollinhofer@gmail.com>
2011-03-27 23:37:36 -0400

    uploading 032711

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,9 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
   
-  validates :name, :presence => true
+  validates :name,  :presence   => true
+                    :length     => { :maximum => 50 }
+  validates :email, :presence   => true
+                    :format     => { :with => email_regex }
+                    :uniqueness => { :case_sensitive => false}
 end
